daaf1574b7235f2b2f0146319949dca817f58304
Iggip <81736777+Iggip@users.noreply.github.com>
2021-05-08 20:05:30 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,130 @@
+TOKEN = 'YOUR_TOKEN'
+bot_activity = 'STATUS'
+prefix = 'COMMAND_PREFIX(/, !, ?...)'
+username = 'YOUR_USERNAME_ON_pdfcrowd.com'
+api_token = 'YOUR_API_TOKEN_ON_pdfcrowd.com'
+part1 = '''
+<html style="font-size: 100%; background-color:#36393f;" data-rh="lang,style,class" lang="uk">
+<head>
+    <style>body,
+    div,
+    h2,
+    html,
+    img,
+    span {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-weight: inherit;
+        font-style: inherit;
+        font-family: inherit;
+        font-size: 100%;
+        vertical-align: baseline;
+    }
+
+    :root {
+        --nickname-color: #71376b;
+        --time-color: #72767d;
+        --message-color: #dcddde;
+    }
+
+
+    body {
+        font-family: Whitney, Helvetica Neue, Helvetica, Arial, sans-serif;
+        text-rendering: optimizeLegibility;
+    }
+
+
+    .wrapper {
+        padding-top: 0.125rem;
+        padding-bottom: 0.125rem;
+        padding-left: 72px;
+    }
+
+
+    .avatar {
+        position: absolute;
+        left: 16px;
+        margin-top: calc(4px - 0.125rem);
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        overflow: hidden;
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        -webkit-box-flex: 0;
+        -ms-flex: 0 0 auto;
+        flex: 0 0 auto;
+        pointer-events: none;
+        z-index: 1;
+    }
+
+
+    .nickname {
+        color: var(--nickname-color);
+        font-family: Whitney, Helvetica Neue, Helvetica, Arial, sans-serif;
+    }
+
+    .time {
+        display: inline-block;
+        height: 1.25rem;
+        cursor: default;
+        pointer-events: none;
+        font-weight: 500;
+        margin-left: 0.25rem;
+        font-size: 0.75rem;
+        line-height: 1.375rem;
+        color: var(--time-color);
+        vertical-align: baseline;
+    }
+
+
+    .separator {
+        position: absolute;
+        opacity: 0;
+        width: 0;
+        display: inline-block;
+        font-style: normal;
+    }
+
+
+    .message {
+        font-size: 1rem;
+        line-height: 1.375rem;
+        word-wrap: break-word;
+        -webkit-user-select: text;
+        -moz-user-select: text;
+        -ms-user-select: text;
+        user-select: text;
+        color: var(--message-color);
+        font-weight: 400;
+    }
+    </style>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="icon" href="https://i.imgur.com/nIiHvN1.png">
+    <title>SAS</title>
+    <script src="moz-extension://2d9fef05-ce8e-45b8-b93d-ad340d727c4e/assets/prompt.js"></script>
+</head>
+<body>
+<div class="wrapper">
+    <div>
+        <img src="https://cdn.discordapp.com/avatars/428159463002734595/efae82bb3a59509d27eb222fe91713a0.png?size=128"
+             class="avatar" alt="">
+        <h2 class="header-23xsNx"><span class="headerText-3Uvj1Y"><span class="nickname">SAS</span></span><span
+                class="time"><i class="separator"> — </i>
+'''
+part2 = '''
+</span></h2>
+        <div class="message">
+'''
+part3 = '''
+</div>
+    </div>
+</div>
+</body>
+</html>
+'''
